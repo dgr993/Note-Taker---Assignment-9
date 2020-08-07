d09c151a239d77689553f8543d99caa00b8a7e73
Wuzup <dgr993@gmail.com>
2020-08-07 09:24:11 -0600

    finished with app.get and app.post but working on app.delete almost done jsut need to save it

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
 var fs = require("fs");
 var myJson = require("./db/db.json")
-let notes = []
-fs.writeFile( "db/db.json", JSON.stringify( myJson ), function (err) {
+let notes = [{
+
+}]
+
+fs.writeFile("db/db.json", JSON.stringify(myJson), function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
@@ -20,35 +23,73 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/assets/js/index.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+});
+
+app.get("/assets/css/styles.css", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
+});
+
+// API's
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    return res.json(notes);
+  })
+});
 
 
-// // API's
-//   app.get("/api/notes", function(req, res) {
-//     fs.readFile(__dirname + "./db/db.json", function(err, data) {
-//       if (err) throw err;
-//       res.end(data);
-//       let notes = JSON.parse(data);
-//       return res.json(notes);
-//   });
+app.post("/api/notes", function (req, res) {
 
-  app.post("/api/notes", function(req, res) {
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    let notes = JSON.parse(data);
     var newNote = req.body;
-    console.log(newNote);
+    newNote.id = newNote.title;
     notes.push(newNote);
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
+    if (err) throw err;
+    console.log(newNote);
     res.json(newNote);
-  });
+    })
+  })
+});
 
-  app.get("/api/notes:id", function(req, res) {
-    return res.json(characters);
-  });
+app.delete("/api/notes/:id", function (req, res) {
+  var notesID = req.params.id;
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    var newNote = req.body;
+    console.log(notes);
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+  for (var i = 0; i < notes.length; i++) {
+    if (notesID === notes[i].text) {
+      let newNote = notes.splice(i, 1) 
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
+        if (err) throw err;
+        console.log(newNote);
+        res.json(newNote);
+        })
+      return res.json(notes[i]);
+    }
+  }
+})
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  return res.json(false);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
